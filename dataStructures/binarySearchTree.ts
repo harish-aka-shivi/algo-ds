df9fe6c1df27a36f406c4f7bf26dbcb66a39710d
Harish <r.shivi@yahoo.com>
2023-05-01 13:48:20 +0530

    feat: add bst methods

--- dataStructures/binarySearchTree.ts
+++ dataStructures/binarySearchTree.ts
@@ -33,7 +33,44 @@ const insertNode = (root: BinarySearchTreeNode, value: number): void => {
   }
 };
 
-function bstFromPreorder(preOrder: number[]): BinarySearchTreeNode | null {
+const findMinValue = (root: BinarySearchTreeNode | null): BinarySearchTreeNode | null => {
+  if (root === null) {
+    return null;
+  }
+  const ret = findMinValue(root.left);
+  return ret === null ? root : ret;
+};
+
+const deleteNode = (root: BinarySearchTreeNode | null, key: number): BinarySearchTreeNode | null => {
+  if (root === null) {
+    return null;
+  }
+
+  if (key < root.value) {
+    root.left = deleteNode(root.left, key);
+  } else if (key > root.value) {
+    root.right = deleteNode(root.right, key);
+  } else {
+    if (root.left === null && root.right === null) {
+      root = null;
+    } else if (root.left === null) {
+      root = root.right;
+    } else if (root.right === null) {
+      root = root.left;
+    } else {
+      const nextMinRoot = findMinValue(root.right);
+      const nextMinValue = nextMinRoot?.value ?? null;
+      if (nextMinValue !== null) {
+        root.value = nextMinValue;
+        root.right = deleteNode(root.right, nextMinValue);
+      }
+    }
+  }
+
+  return root;
+};
+
+const bstFromPreorder = (preOrder: number[]): BinarySearchTreeNode | null => {
   let i = 1;
   const root = new BinarySearchTreeNode(preOrder[0]);
   while (i < preOrder.length) {
@@ -42,4 +79,54 @@ function bstFromPreorder(preOrder: number[]): BinarySearchTreeNode | null {
   }
 
   return root;
+};
+
+const invertTree = (root: BinarySearchTreeNode | null): BinarySearchTreeNode | null => {
+  if (root === null) {
+    return null;
+  }
+
+  const left = invertTree(root.left);
+  const right = invertTree(root.right);
+  root.left = right;
+  root.right = left;
+
+  return root;
+};
+
+const isValid = (
+  root: BinarySearchTreeNode | null,
+): {
+  max: number | null;
+  min: number | null;
+  isValidSubtrees: boolean;
+} => {
+  if (root === null) {
+    return {
+      isValidSubtrees: true,
+      min: null,
+      max: null,
+    };
+  }
+
+  const { isValidSubtrees: isLeftValid, max: leftMax, min: leftMin } = isValid(root.left);
+  const { isValidSubtrees: isRightValid, max: rightMax, min: rightMin } = isValid(root.right);
+
+  const tempLeftMax = leftMax === null ? Number.MIN_SAFE_INTEGER : leftMax;
+  const tempRightMax = rightMax === null ? Number.MIN_SAFE_INTEGER : rightMax;
+  const treeMaxAtRoot = Math.max(tempLeftMax, tempRightMax, root.value);
+
+  const tempLeftMin = leftMin === null ? Number.MAX_SAFE_INTEGER : leftMin;
+  const tempRightMin = rightMin === null ? Number.MAX_SAFE_INTEGER : rightMin;
+  const treeMinAtRoot = Math.min(tempLeftMin, tempRightMin, root.value);
+
+  return {
+    isValidSubtrees: isLeftValid && isRightValid && root.value > tempLeftMax && root.value < tempRightMin,
+    max: treeMaxAtRoot,
+    min: treeMinAtRoot,
+  };
+};
+
+function isValidBST(root: BinarySearchTreeNode | null): boolean {
+  return isValid(root).isValidSubtrees;
 }
--- dataStructures/binaryTree.ts
+++ dataStructures/binaryTree.ts
@@ -66,16 +66,171 @@ const buildOrderMap = (root: BinaryTreeNode | null, level: number, map: Record<s
   buildOrderMap(root.right, level + 1, map);
 };
 
-function levelOrder(root: BinaryTreeNode | null): number[][] {
+const levelOrder = (root: BinaryTreeNode | null): number[][] => {
   const map = {};
   buildOrderMap(root, 0, map);
   return Object.values(map);
+};
+
+const buildLevelOrderTree = (arr: number[]): BinaryTreeNode | null => {
+  const zeroValue = arr.shift();
+  if (zeroValue === undefined) {
+    return null;
+  }
+  const root = new BinaryTreeNode(zeroValue);
+  let level = 1;
+  const map = { 0: [root] } as Record<string, (BinaryTreeNode | null)[]>;
+  while (arr.length !== 0) {
+    for (let i = 0; i < Math.pow(2, level); i++) {
+      const val = arr.shift();
+      if (val !== undefined) {
+        let node = null;
+        if (val !== -1) {
+          node = new BinaryTreeNode(val);
+        }
+        if (map[level]) {
+          map[level].push(node);
+        } else {
+          map[level] = [node];
+        }
+
+        const minusOneLevelNodes = map[level - 1];
+        const parentIndex = Math.trunc(i / 2);
+        const parent = minusOneLevelNodes[parentIndex];
+        if (parent) {
+          if (i % 2 === 0) {
+            parent.left = node;
+          } else {
+            parent.right = node;
+          }
+        }
+      }
+    }
+    level++;
+  }
+  return root;
+};
+
+const buildLevelOrderTree2 = (arr: number[]): BinaryTreeNode | null => {
+  const zeroValue = arr.shift();
+  if (zeroValue === undefined) {
+    return null;
+  }
+  const root = new BinaryTreeNode(zeroValue);
+  const queue: (BinaryTreeNode | null)[] = [root];
+  while (queue.length !== 0) {
+    const topNode = queue.shift();
+
+    const nextLeft = arr.shift();
+    const nextRight = arr.shift();
+    if (nextLeft !== undefined && nextRight !== undefined && !!topNode) {
+      const leftNode = nextLeft !== -1 ? new BinaryTreeNode(nextLeft) : null;
+      const rightNode = nextRight !== -1 ? new BinaryTreeNode(nextRight) : null;
+
+      queue.push(leftNode);
+      queue.push(rightNode);
+
+      topNode.left = leftNode;
+      topNode.right = rightNode;
+    }
+  }
+  return root;
+};
+
+const depth = (root: BinaryTreeNode | null): number => {
+  if (root === null) {
+    return 0;
+  }
+  const leftHeight = 1 + depth(root.left);
+  const rightHeight = 1 + depth(root.right);
+
+  return Math.max(leftHeight, rightHeight);
+};
+
+/* 
+    UnOptimized
+*/
+// function (root: TreeNode | null): number {
+//     if (root === null) {
+//         return 0
+//     }
+//     const widthPassingThroughRoot = height(root.left) + height(root.right)
+//     const diameterLeft = diameterOfBinaryTree(root.left)
+//     const diameterRight = diameterOfBinaryTree(root.right)
+
+//     return Math.max(widthPassingThroughRoot, Math.max(diameterLeft, diameterRight))
+// };
+
+const calculateDiameter = (root: BinaryTreeNode | null): { depth: number; diameter: number } => {
+  if (root === null) {
+    return {
+      depth: 0,
+      diameter: 0,
+    };
+  }
+  const objLeft = calculateDiameter(root.left);
+  const objRight = calculateDiameter(root.right);
+
+  const leftHeight = objLeft.depth;
+  const rightHeight = objRight.depth;
+  const diameterLeft = objLeft.diameter;
+  const diameterRight = objRight.diameter;
+  const widthPassingThroughRoot = leftHeight + rightHeight;
+  return {
+    depth: 1 + Math.max(leftHeight, rightHeight),
+    diameter: Math.max(widthPassingThroughRoot, Math.max(diameterLeft, diameterRight)),
+  };
+};
+
+// optimised diameter
+const diameter = (root: BinaryTreeNode | null): number => {
+  return calculateDiameter(root).diameter;
+};
+
+const calcMinDepth = (root: BinaryTreeNode | null, level: number, levels: number[]) => {
+  if (root === null) {
+    return 0;
+  }
+  if (root.left === null && root.right === null) {
+    levels.push(level);
+  }
+  calcMinDepth(root.left, level + 1, levels);
+  calcMinDepth(root.right, level + 1, levels);
+};
+
+function minDepth(root: BinaryTreeNode | null): number {
+  const levels: number[] = [];
+
+  calcMinDepth(root, 1, levels);
+
+  let min = levels[0] ?? 0;
+
+  for (const val of levels) {
+    min = val < min ? val : min;
+  }
+
+  return min;
 }
 
 const input = [1, 2, 4, -1, -1, 5, 7, -1, -1, -1, 3, -1, 6, -1, -1];
+const inputLevelOrder = [1, 2, 3, 4, 5, -1, 6, -1, -1, 7, -1, -1, -1, -1, -1];
+const inputLevelOrder2 = [1, 2, 3, 4, 5, -1, 6, -1, -1, 7, -1, -1, -1, -1, -1];
+
 const root = buildTreePreOrder(input);
 
 console.log(printPreOrder(root));
 console.log(printInOrder(root));
 console.log(printPostOrder(root));
 console.log(levelOrder(root));
+
+const tree2 = buildLevelOrderTree(inputLevelOrder);
+console.log(printPreOrder(tree2));
+console.log(printInOrder(tree2));
+console.log(printPostOrder(tree2));
+console.log(levelOrder(tree2));
+
+const tree3 = buildLevelOrderTree2(inputLevelOrder2);
+console.log(printPreOrder(tree3));
+console.log(printInOrder(tree3));
+console.log(printPostOrder(tree3));
+console.log(levelOrder(tree3));
